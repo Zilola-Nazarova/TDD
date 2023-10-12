35bef29a003f491c828cf78de9037ee8ec94d4a6
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-12 15:35:13 +0500

    Fix rubocop errors

--- solver.rb
+++ solver.rb
@@ -1,11 +1,11 @@
 class Solver
-  def factorial(n)
-    if n == 0
+  def factorial(num)
+    if num.zero?
       1
-    elsif n > 0
-      n * factorial(n - 1)
-    elsif n < 0
-      raise StandardError.new('Can not calculate facotrial for negative integer')
+    elsif num.positive?
+      num * factorial(num - 1)
+    elsif num.negative?
+      raise StandardError, 'Can not calculate facotrial for negative integer'
     end
   end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,17 +3,17 @@ require_relative '../solver'
 describe Solver do
   describe '#factorial' do
     before :all do
-      @N = Solver.new
+      @num = Solver.new
     end
 
     it 'should return factorial for a positive number' do
-      expect(@N.factorial(4)).to eql 24
+      expect(@num.factorial(4)).to eql 24
     end
     it 'should return 1 for a number 0 (special case)' do
-      expect(@N.factorial(0)).to eql 1
+      expect(@num.factorial(0)).to eql 1
     end
     it 'should raise an exception for a negative integer' do
-      expect{ @N.factorial(-2) }.to raise_error('Can not calculate facotrial for negative integer', StandardError)
+      expect { @num.factorial(-2) }.to raise_error('Can not calculate facotrial for negative integer', StandardError)
     end
   end
 
@@ -29,16 +29,16 @@ describe Solver do
 
       end
       it 'should return "fizz" for 6' do
-  
+
       end
     end
-    
+
     context 'divisible by 5' do
       it 'should return "buzz" for 5' do
 
       end
       it 'should return "buzz" for 10' do
-  
+
       end
     end
 
@@ -47,7 +47,7 @@ describe Solver do
 
       end
       it 'should return "fizzbuzz" for 30' do
-  
+
       end
     end
 
@@ -56,7 +56,7 @@ describe Solver do
 
       end
       it 'should return "13" for 13' do
-  
+        
       end
     end
   end
